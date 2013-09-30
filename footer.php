d65fc8dac5b1408ba992a391839f728ae43b8ee6
Alvin Grant <alving.nyc@gmail.com>
2013-09-30 17:37:28 -0400

    Add home link to brand logo
    Add theme support for widgets in the footer area

--- footer.php
+++ footer.php
@@ -1,9 +1,26 @@
 	</div>
 </body>
 <footer>
-	<?php 
-		if (! is_404() )
-			get_sidebar('footer');
-	?>
+	<div id="footer-sidebar-1">
+		<?php
+			if(is_active_sidebar('footer-sidebar-1')){
+			dynamic_sidebar('footer-sidebar-1');
+			}
+		?>
+	</div>
+	<div id="footer-sidebar-2">
+		<?php
+			if(is_active_sidebar('footer-sidebar-2')){
+			dynamic_sidebar('footer-sidebar-2');
+			}
+		?>
+	</div>
+	<div id="footer-sidebar-3">
+		<?php
+			if(is_active_sidebar('footer-sidebar-3')){
+			dynamic_sidebar('footer-sidebar-3');
+			}
+		?>
+	</div>
 	<a href="#">Copyright &copy 2013 - GC4Women - All Rights Reserved.</a>
 </footer>
--- functions.php
+++ functions.php
@@ -11,6 +11,38 @@
 	if ( function_exists('register_sidebar') )
 		register_sidebar();
 
+	//Widget enabled footer
+	function gc4w_widget_init(){
+		register_sidebar(array(
+		'name' => 'Footer Siderbar 1',
+		'id' => 'footer-sidebar-1',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title',
+		'after_title' => '</h3>'
+		));
+		register_sidebar(array(
+			'name' => 'Footer Siderbar 2',
+			'id' => 'footer-sidebar-2',
+			'description' => 'Appears in the footer area',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget' => '</aside>',
+			'before_title' => '<h3 class="widget-title',
+			'after_title' => '</h3>'
+		));
+		register_sidebar(array(
+			'name' => 'Footer Siderbar 3',
+			'id' => 'footer-sidebar-3',
+			'description' => 'Appears in the footer area',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget' => '</aside>',
+			'before_title' => '<h3 class="widget-title',
+			'after_title' => '</h3>'
+		));
+	}
+	add_action('widgets_init','gc4w_widget_init')
+
 	//Custom Backgrounds
-	add_theme_support('custom-background');
+	//add_theme_support('custom-background');
 ?>
