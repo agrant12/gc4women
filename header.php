a4a05ebf07ac920ad324c5291da2e7a49bba0439
Alvin Grant <alving.nyc@gmail.com>
2013-10-02 18:25:56 -0400

    Define layout of basic blog view
    Rename "blog" div to "main"
    
    Define title in browser tabs with site name and current page separated by pipe symbol
    
    Remove height for header

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<title><?php wp_title('|', true, 'right' ); ?></title>
+<title><?php bloginfo('name'); ?> <?php wp_title('|', true, 'left' ); ?></title>
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
