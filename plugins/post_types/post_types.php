1583ea0ab867920e2be6c57342cd5bef0b5b47d4
Alvin Grant <alving.nyc@gmail.com>
2013-10-06 21:02:43 -0400

    Create a custom post type plugin
    Add "spotlight" as a registered post type

---
+++ plugins/post_types/post_types.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+Plugin Name: GC4W Custom Post Types
+Version: 1.0
+Plugin URI: http://gc4women.org
+Description: Set custom posttypes
+Author: Alvin Grant
+Author URI: http://alvingrant.com
+*/
+
+function my_custom_post_spotlight() {
+	$labels = array(
+		'name'               => _x( 'Spotlight', 'post type general name' ),
+		'singular_name'      => _x( 'Spotlight', 'post type singular name' ),
+		'add_new'            => _x( 'Add New', 'Spotlight Story' ),
+		'add_new_item'       => __( 'Add New Spotlight' ),
+		'edit_item'          => __( 'Edit Spotlight' ),
+		'new_item'           => __( 'New Spotlight' ),
+		'all_items'          => __( 'All Spotlights' ),
+		'view_item'          => __( 'View Spotlight' ),
+		'search_items'       => __( 'Search Spotlights' ),
+		'not_found'          => __( 'No spotlights found' ),
+		'not_found_in_trash' => __( 'No spotlights found in the Trash' ), 
+		'parent_item_colon'  => '',
+		'menu_name'          => 'Spotlights'
+	);
+	$args = array(
+		'labels'        => $labels,
+		'description'   => 'Contains spotlight stories',
+		'public'        => true,
+		'menu_position' => 6,
+		'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+		'has_archive'   => true,
+	);
+	register_post_type( 'spotlight', $args );	
+}
+add_action( 'init', 'my_custom_post_spotlight' );
+
+?>
