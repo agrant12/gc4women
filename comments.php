b3d78806af889144d583df34841fd99d4ea8c18a
agrant12 <alving.nyc@gmail.com>
2013-11-04 16:24:26 -0500

    Display comments under post
    Change title_reply text

--- comments.php
+++ comments.php
@@ -1,1 +1,26 @@
-<?php comment_form(); ?>
+<?php $form = array(
+	'title_reply'		=> 'Join the Discussion',
+	);
+?>
+
+<?php comment_form($form); ?>
+
+<?php $arg = array(
+	'walker'            => null,
+	'max_depth'         => '',
+	'style'             => 'ul',
+	'callback'          => null,
+	'end-callback'      => null,
+	'type'              => 'all',
+	'reply_text'        => 'Post a Reply',
+	'page'              => '',
+	'per_page'          => '',
+	'avatar_size'       => 64,
+	'reverse_top_level' => null,
+	'reverse_children'  => '',
+	'format'            => 'xhtml', //or html5 @since 3.6
+	'short_ping'        => false // @since 3.6)
+	);
+?>
+
+<?php wp_list_comments($arg, $comments); ?> 
