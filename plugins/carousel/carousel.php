8a858203b5434e41a52ae83a12eabc78fd2d6fd0
Alvin Grant <alving.nyc@gmail.com>
2014-11-27 14:00:09 -0500

    Escape attributes and html

--- plugins/carousel/carousel.php
+++ plugins/carousel/carousel.php
@@ -44,7 +44,7 @@ function carousel() {
 					?>
 					<li>
 						<div class="thumbnail">
-							<a href="<?php echo get_permalink($slide); ?>"><img src="<?php echo MultiPostThumbnails::get_post_thumbnail_url($post_type, 'secondary-image', $post_id = $slide, 'carousel-image'); ?>" /></a>
+							<a href="<?php echo esc_url(get_permalink($slide)); ?>"><img src="<?php echo esc_url(MultiPostThumbnails::get_post_thumbnail_url($post_type, 'secondary-image', $post_id = $slide, 'carousel-image')); ?>" /></a>
 						</div>
 					</li>
 				<?php endforeach; ?>
