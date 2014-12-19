598e4b94213a018e76981a0236f62b11a26915de
Alvin Grant <alving.nyc@gmail.com>
2014-12-19 10:52:11 -0500

    Update page.php template to pick up proper css

--- page.php
+++ page.php
@@ -1,23 +1,21 @@
 <?php get_header(); ?>
-    <div class="container">
-        <div class="main">
-            <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+	<div class="main">
+		<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 
-            <div class="post">
-               <div class="entry">
-                    <?php the_content(); ?>
+		<div class="post">
+			<div class="entry">
+				<?php the_content(); ?>
 
-                    <p class="postmetadata">
-                        <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
-                        <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
-                    </p>
-                </div>
-            </div>
+				<p class="postmetadata">
+					<?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?> <?php  the_author(); ?><br />
+					<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?> <?php edit_post_link('Edit', ' &#124; ', ''); ?>
+				</p>
+			</div>
+		</div>
 
-            <?php endwhile; ?>
+		<?php endwhile; ?>
 
-            <?php endif; ?>
-        </div>
-        <?php get_sidebar(); ?> 
-    </div>  
+		<?php endif; ?>
+	</div>
+	<?php get_sidebar(); ?> 
 <?php get_footer(); ?>
